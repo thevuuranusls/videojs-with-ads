6aa747c353a16835897ac4c7d1e1c8ff1dec8bd6
Victor <victor@streamroot.io>
2016-10-12 14:30:42 +0200

    Add fallback for clients with an adblocker

--- ads.js
+++ ads.js
@@ -12,62 +12,64 @@ var Ads = function(videoid, source) {
           autoplay: false
         };
 
-       var adOptions = {
-          id: videoid,
-          adTagUrl: this.AdTagInput,
-          showCountdown: true,
-          adLabel: "Publicité",
-          vpaidMode: google.ima.ImaSdkSettings.VpaidMode.ENABLED
-      };
+        this.source = source;
+        this.player = videojs(videoid, this.options);
+        var pl = this.player;
 
-      this.source = source;
-      this.player = videojs(videoid, this.options);
-
-      // Remove controls from the player on iPad to stop native controls from stealing
-      // our click
-      var contentPlayer =  document.getElementById(videoid + '_html5_api');
-      if ((navigator.userAgent.match(/iPad/i) ||
-              navigator.userAgent.match(/Android/i)) &&
-          contentPlayer.hasAttribute('controls')) {
-        contentPlayer.removeAttribute('controls');
-      }
-
-      this.events = [
-        google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
-        google.ima.AdEvent.Type.CLICK,
-        google.ima.AdEvent.Type.COMPLETE,
-        google.ima.AdEvent.Type.FIRST_QUARTILE,
-        google.ima.AdEvent.Type.LOADED,
-        google.ima.AdEvent.Type.MIDPOINT,
-        google.ima.AdEvent.Type.PAUSED,
-        google.ima.AdEvent.Type.STARTED,
-        google.ima.AdEvent.Type.THIRD_QUARTILE
-      ];
-
-      var pl = this.player;
-
-      pl.ima(
-          adOptions,
-          this.bind(this, this.adsManagerLoadedCallback)
-      );
-
-      // adding the Sound on mouse over option:
-     pl.on('mouseover', function(){
-       pl.volume(1);
-     });
-     pl.on('mouseout', function(){
+        // adding the Sound on mouse over option:
+       pl.on('mouseover', function(){
+         pl.volume(1);
+       });
+       pl.on('mouseout', function(){
+         pl.volume(0);
+       });
        pl.volume(0);
-     });
-     pl.volume(0);
 
-     this.init();
+      try{
+          var adOptions = {
+              id: videoid,
+              adTagUrl: this.AdTagInput,
+              showCountdown: true,
+              adLabel: "Publicité",
+              vpaidMode: google.ima.ImaSdkSettings.VpaidMode.ENABLED
+          };
+
+          // Remove controls from the player on iPad to stop native controls from stealing
+          // our click
+          var contentPlayer =  document.getElementById(videoid + '_html5_api');
+          if ((navigator.userAgent.match(/iPad/i) ||
+                  navigator.userAgent.match(/Android/i)) &&
+              contentPlayer.hasAttribute('controls')) {
+            contentPlayer.removeAttribute('controls');
+          }
+
+          this.events = [
+            google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
+            google.ima.AdEvent.Type.CLICK,
+            google.ima.AdEvent.Type.COMPLETE,
+            google.ima.AdEvent.Type.FIRST_QUARTILE,
+            google.ima.AdEvent.Type.LOADED,
+            google.ima.AdEvent.Type.MIDPOINT,
+            google.ima.AdEvent.Type.PAUSED,
+            google.ima.AdEvent.Type.STARTED,
+            google.ima.AdEvent.Type.THIRD_QUARTILE
+          ];
+
+          pl.ima(
+              adOptions,
+              this.bind(this, this.adsManagerLoadedCallback)
+          );
+
+          this.player.ima.setContentWithAdTag(this.source, this.AdTagInput, false);
+          this.player.ima.requestAds();
+     }
+     catch(err) {
+         this.player.src(this.source);
+     }
+     finally {
+         this.player.play();
+     }
  };
-
-Ads.prototype.init = function() {
-    this.player.ima.setContentWithAdTag(this.source, this.AdTagInput, false);
-    this.player.ima.requestAds();
-    this.player.play();
-};
 
 Ads.prototype.adsManagerLoadedCallback = function() {
   for (var index = 0; index < this.events.length; index++) {
